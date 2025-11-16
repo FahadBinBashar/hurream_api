7f79f4982b58d392254651a9ccf586f3fcd4d460
FahadBinBashar <fuadbinbashar1990@gmail.com>
2025-11-16 17:49:15 +0600

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return "HI Hurream";
 });
+
+Route::get('/clear-cache', function () {
+
+    Artisan::call('optimize:clear');   // clears everything
+    Artisan::call('cache:clear');      // application cache
+    Artisan::call('config:clear');     // config cache
+    Artisan::call('route:clear');      // route cache
+    Artisan::call('view:clear');       // view cache
+    Artisan::call('clear-compiled');   // compiled classes
+
+    return "<h2>All Cache Cleared Successfully!</h2>";
+});
